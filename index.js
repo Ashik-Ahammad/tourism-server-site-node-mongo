de8ba662b53f4d738e728dcdff05cfb8b667219e
Ashik-Ahammad <ashik19ahammad@gmail.com>
2021-10-31 22:24:38 +0600

    server pushed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,9 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//user - marsTravelUser
-//pass - kpV83C1feSHe8DIV
-// hello
+
 
 const uri = "mongodb+srv://marsTravelUser:kpV83C1feSHe8DIV@cluster0.xd7k8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
@@ -23,6 +24,7 @@ async function run() {
         await client.connect()
         const database = client.db('mars_Travel')
         const packageCollection = database.collection('packages');
+        const userCollection = database.collection('users');
 
 
         //GET PACKAGES API
@@ -31,11 +33,51 @@ async function run() {
             const packages = await cursor.toArray();
             res.send(packages)
         })
+        //GET Users API
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+        //Get Single Package
+        app.get('/packages/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const package = await packageCollection.findOne(query);
+            res.json(package);
+        })
+
+        //POST API
+        app.post('/packages', async (req, res) => {
+            const service = req.body;
+            console.log('post api hitted', service);
+            const result = await packageCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+        })
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log('2nd post api hitted', user);
+            const result = await userCollection.insertOne(user);
+            console.log(result);
+            res.json(result)
+        })
+
+        // Delete API
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            console.log('Deleted', result);
+            res.json(result);
+        })
     }
     finally {
         // await client.close();
     }
 }
+
 
 run().catch(console.dir)
 
